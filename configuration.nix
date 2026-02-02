dd48546817274c7be911709f372c7eeb1f82703f
yortug <29676223+yortug@users.noreply.github.com>
2026-02-02 13:35:35 +0800

    one of the directory_node's CAN connect, but massive ping, so blocks everything ... should really look into the other implementation which claims to solve this - hmmm

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@ let
       #max_cj_fee_rel = "0.003";
     };
     "MESSAGING:onion" = {
-      directory_nodes = "jmarketxf5wc4aldf3slm5u6726zsky52bqnfv6qyxe5hnafgly6yuyd.onion:5222,coinjointovy3eq5fjygdwpkbcdx63d7vd4g32mw7y553uj3kjjzkiqd.onion:5222,satoshi2vcg5e2ept7tjkzlkpomkobqmgtsjzegg6wipnoajadissead.onion:5222,nakamotourflxwjnjpnrk7yc2nhkf6r62ed4gdfxmmn5f4saw5q5qoyd.onion:5222,odpwaf67rs5226uabcamvypg3y4bngzmfk7255flcdodesqhsvkptaid.onion:5222"; 
+      directory_nodes = "jmarketxf5wc4aldf3slm5u6726zsky52bqnfv6qyxe5hnafgly6yuyd.onion:5222,coinjointovy3eq5fjygdwpkbcdx63d7vd4g32mw7y553uj3kjjzkiqd.onion:5222,satoshi2vcg5e2ept7tjkzlkpomkobqmgtsjzegg6wipnoajadissead.onion:5222,nakamotourflxwjnjpnrk7yc2nhkf6r62ed4gdfxmmn5f4saw5q5qoyd.onion:5222"; 
     };
   };
   
